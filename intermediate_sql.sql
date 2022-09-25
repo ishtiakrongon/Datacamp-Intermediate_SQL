acd7f235ec19824f0882c1168b75fd8c295bb6a2
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-09-25 17:23:34 +0600

    Update intermediate_sql.sql

--- intermediate_sql.sql
+++ intermediate_sql.sql
@@ -67,3 +67,10 @@ SELECT title, release_year
 FROM films
 WHERE language = 'German'
 	AND release_year < 2000;
+
+-- Select all records for German-language films released after 2000 and before 2010
+
+SELECT *
+FROM films
+WHERE language = 'German'
+    AND (release_year > 2000 AND release_year < 2010);
