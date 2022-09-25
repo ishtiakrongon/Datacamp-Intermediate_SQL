cf9016828fcd992af57602ff835be148ef0ece57
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-09-25 23:02:18 +0600

    Update intermediate_sql.sql

--- intermediate_sql.sql
+++ intermediate_sql.sql
@@ -206,3 +206,24 @@ SELECT MIN(duration) AS shortest_film
 FROM films;
 
 
+-- Calculate the sum of gross from the year 2000 or later
+
+SELECT SUM(gross) AS total_gross
+FROM films
+WHERE release_year >= 2000;
+
+
+-- Calculate the average gross of films that start with A
+
+SELECT AVG(gross) AS avg_gross_A
+FROM films
+WHERE title LIKE 'A%';
+
+
+-- Calculate the lowest gross film in 1994
+
+SELECT MIN(gross) AS lowest_gross
+FROM films
+WHERE release_year = 1994;
+
+
