1f90e5fc23e35b57f56a8b46946bf5e66265e134
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-09-25 15:55:37 +0600

    Update intermediate_sql.sql

--- intermediate_sql.sql
+++ intermediate_sql.sql
@@ -1,9 +1,22 @@
 -- Intermediate SQL
+-- CHAPTER 1 | SELECTING DATA
+-------------------------------------------------------------------------------------------
 
 -- Practice with COUNT()
 -- Count the number of records in the people table
 
 SELECT COUNT(*) AS count_records
 FROM people;
+
+-- Count the number of birthdates in the people table
+
+SELECT COUNT(birthdate) AS count_birthdate
+FROM people;
+
+-- Count the languages and countries represented in the films table
+
+SELECT COUNT(language) AS count_languages, COUNT(country) AS count_countries
+FROM films;
+
 
 
