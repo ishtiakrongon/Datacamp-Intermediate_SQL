3438bfa19ba3f2327d1f098e5071c5c4b11376bb
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-09-25 22:46:42 +0600

    chapter 3 | Aggregate functions

--- intermediate_sql.sql
+++ intermediate_sql.sql
@@ -178,3 +178,31 @@ WHERE budget IS NULL;
 SELECT COUNT(*) AS count_language_known
 FROM films
 WHERE language IS NOT NULL;
+
+
+-----------------------------------------------------------------------------------------
+
+-- CHAPTER 3 | AGGREAGATE FUNCTIONS
+
+-- Query the sum of film durations
+SELECT SUM(duration) AS total_duration
+FROM films;
+
+
+-- Calculate the average duration of all films
+SELECT AVG(duration) AS average_duration
+FROM films;
+
+
+-- Find the latest release_year
+
+SELECT MAX(release_year) AS latest_year
+FROM films;
+
+
+-- Find the duration of the shortest film
+
+SELECT MIN(duration) AS shortest_film
+FROM films;
+
+
