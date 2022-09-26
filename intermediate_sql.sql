4650d2af7ffefa907ad452f437062601c15ffea4
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-09-26 21:20:01 +0600

    Update intermediate_sql.sql

--- intermediate_sql.sql
+++ intermediate_sql.sql
@@ -290,3 +290,17 @@ FROM films
 GROUP BY release_year;
 
 
+-- Find the release_year and average duration of films for each year
+
+SELECT release_year, AVG(duration) AS avg_duration
+FROM films
+GROUP BY release_year;
+
+
+-- Find the release_year, country, and max_budget, then group and order by release_year and country
+
+SELECT release_year, country, MAX(budget) AS max_budget
+FROM films
+GROUP BY release_year, country
+ORDER BY release_year, country;
+
