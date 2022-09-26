9810ebc38a1d3555b0594507acecb8f7a3b1f694
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-09-26 21:25:10 +0600

    Update intermediate_sql.sql

--- intermediate_sql.sql
+++ intermediate_sql.sql
@@ -304,3 +304,11 @@ FROM films
 GROUP BY release_year, country
 ORDER BY release_year, country;
 
+-- Which release_year had the most language diversity?
+
+SELECT release_year, COUNT(DISTINCT language) AS language_diversity
+FROM films
+GROUP BY release_year
+ORDER BY language_diversity DESC;
+
+
