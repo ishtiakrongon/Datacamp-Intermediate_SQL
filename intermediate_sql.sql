0242f9e028a4c41c320837757e98e2df28cca79f
Md. Ishtiak Ahmed <42693745+ishtiakrongon@users.noreply.github.com>
2022-09-26 21:16:59 +0600

    Update intermediate_sql.sql

--- intermediate_sql.sql
+++ intermediate_sql.sql
@@ -284,3 +284,9 @@ FROM films
 ORDER BY certification ASC, release_year DESC;
 
 
+-- Find the release_year and film_count of each year
+SELECT release_year, COUNT(*) AS film_count
+FROM films
+GROUP BY release_year;
+
+
